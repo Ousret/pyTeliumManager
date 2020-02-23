685cff52c404ab2b33f6e00eb9f41d1c237b613b
TAHRI Ahmed R <nyuubi.10@gmail.com>
2020-02-23 14:10:00 +0100

    Use isinstance instead of type in hexdump func

--- telium/hexdump.py
+++ telium/hexdump.py
@@ -6,13 +6,21 @@ def hexdump(src, length=16, sep='.'):
     """
     FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or sep for x in range(256)])
     lines = []
+
     for c in range(0, len(src), length):
         chars = src[c:c + length]
-        hexstr = ' '.join(["%02x" % ord(x) for x in chars]) if type(chars) is str else ' '.join(
+
+        hexstr = ' '.join(["%02x" % ord(x) for x in chars]) if isinstance(chars, str) else ' '.join(
             ['{:02x}'.format(x) for x in chars])
+
         if len(hexstr) > 24:
             hexstr = "%s %s" % (hexstr[:24], hexstr[24:])
-        printable = ''.join(["%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or sep) for x in chars]) if type(
-            chars) is str else ''.join(['{}'.format((x <= 127 and FILTER[x]) or sep) for x in chars])
+
+        printable = ''.join(
+            [
+                "%s" % ((ord(x) <= 127 and FILTER[ord(x)]) or sep) for x in chars
+            ]
+        ) if isinstance(chars, str) else ''.join(['{}'.format((x <= 127 and FILTER[x]) or sep) for x in chars])
+
         lines.append("%08x:  %-*s  |%s|" % (c, length * 3, hexstr, printable))
     print('\n'.join(lines))
