03caff9fa6c201fefa54b7f6bec59058e01c5837
TAHRI Ahmed R <nyuubi.10@gmail.com>
2020-02-28 22:50:33 +0100

    Improve setup.py and expose version in package
    Some python version were missing and the description was not included on PyPi. And fix minors issues.

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
 from setuptools import setup
+import io
+import os
+
+from re import search
+
+
+def get_version():
+    with open('telium/version.py') as version_file:
+        return search(r"""__version__\s+=\s+(['"])(?P<version>.+?)\1""",
+                      version_file.read()).group('version')
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+DESCRIPTION = ('A cross-platform point of sales payment manager tool with Telium Manager '
+               'Support every device with Telium Manager like Ingenico terminals.')
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
 setup(
     name='pyTeliumManager',
-    version='2.4.0',
+    version=get_version(),
     author='Ahmed TAHRI, @Ousret',
     author_email='ahmed.tahri@cloudnursery.dev',
-    description=('A cross-platform point of sales payment manager tool with Telium Manager '
-                 'Support every device with Telium Manager like Ingenico terminals.'),
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     packages=['telium'],
     test_suite='test',
     url='https://github.com/Ousret/pyTeliumManager',
-    download_url='https://github.com/Ousret/pyTeliumManager/archive/2.4.0.tar.gz',
+    download_url='https://github.com/Ousret/pyTeliumManager/archive/2.4.1.tar.gz',
     install_requires=['pyserial>=3.3', 'pycountry>=17.0', 'payment_card_identifier>=0.1.2', 'six'],
     tests_require=['Faker'],
     keywords=['ingenico', 'telium manager', 'telium', 'payment', 'credit card', 'debit card', 'visa', 'mastercard',
@@ -22,7 +45,7 @@ setup(
         'Environment :: X11 Applications',
         'Environment :: MacOS X',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
@@ -33,5 +56,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ]
 )
--- telium/__init__.py
+++ telium/__init__.py
@@ -1,3 +1,4 @@
 from telium.constant import *
 from telium.payment import TeliumAsk, TeliumResponse, LrcChecksumException, SequenceDoesNotMatchLengthException
 from telium.manager import *
+from telium.version import __version__, VERSION
